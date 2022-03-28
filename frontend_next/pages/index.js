4e6d98caa3502d571e4e9f28f395174d08b54dba
Regtables <reghardt7@gmail.com>
2022-03-28 12:13:42 +0200

    testing strapi

--- frontend_next/pages/index.js
+++ frontend_next/pages/index.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Home = () => {
+const Home = ({postsData}) => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    setPosts(postsData.data)
+  }, [])
+
   return (
     <div>
-      Home W
+     <h1>{posts[0]?.attributes?.title}</h1>
+     <h1>{posts[1]?.attributes?.title}</h1>
     </div>
   )
 }
 
 export default Home
 
+export const getStaticProps = async () => {
+  const postReq = await axios.get('http://localhost:1337/api/posts')
+  console.log(postReq)
+  return {
+    props : {
+      postsData: postReq.data,
+    }
+  }
+}
